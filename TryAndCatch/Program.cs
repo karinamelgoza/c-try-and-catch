3a25258d788f8fc6cddd592ac31e1257d96e8127
Karina Melgoza <kmelgoza08@gmail.com>
2021-12-17 12:48:34 -0700

    initial commit

---
+++ TryAndCatch/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TryAndCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a number");
+            string userInput = Console.ReadLine();
+
+            int userInputAsInt = int.Parse(userInput);
+
+            Console.ReadKey();
+        }
+    }
+}
