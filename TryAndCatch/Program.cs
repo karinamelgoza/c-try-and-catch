3e3a51408ee0a6d81e7c81d8daa41e48e2903274
Karina Melgoza <kmelgoza08@gmail.com>
2021-12-17 13:09:16 -0700

    Adds practice with try and catch

--- TryAndCatch/Program.cs
+++ TryAndCatch/Program.cs
@@ -8,8 +8,38 @@ namespace TryAndCatch
         {
             Console.WriteLine("please enter a number");
             string userInput = Console.ReadLine();
+            Console.WriteLine("please enter another number");
+            string userInput2 = Console.ReadLine();
 
-            int userInputAsInt = int.Parse(userInput);
+            try
+            {
+                //int userInputAsInt = int.Parse(userInput);
+                int num1 = int.Parse(userInput);
+                int num2 = int.Parse(userInput2);
+                Console.WriteLine($"result: {num1 / num2}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("format exception, please enter a number");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("overflow exception, the number was too long");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("argument null exception, the value was empty(null)");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("divide by zero exception, do not divide by 0");
+            }
+            finally
+            {
+                Console.WriteLine("This is called anyways");
+            }
+
+
 
             Console.ReadKey();
         }
